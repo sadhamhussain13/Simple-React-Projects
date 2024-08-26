c6ef89bad194bd07b9fd4a329baa8a7aab205a36
Sadham Hussain S <124704197+sadhamhussain13@users.noreply.github.com>
2024-08-26 19:21:38 +0530

    Update main.jsx

--- QR Code Generator/src/main.jsx
+++ QR Code Generator/src/main.jsx
@@ -1,34 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-// import { Advice } from './formSamples/Advice'
-// import { Details } from './formSamples/Details'
-// import { RegForm } from './formSamples/RegForm'
-
-// import App from './App.jsx'
-// import './index.css'
-
 import './QrCode.css'
 import { QrCode } from './QrCode'
 
-// import { UserCard } from './UserCard'
-
-
-// Here note the .. symbol bcz its inside of 2 folders
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <App /> */}
-
-    {/* <UserCard /> */}
 
     <QrCode />
-
-    {/* <Details /> */}
-
-    {/* <RegForm /> */}
-
-    {/* <Advice /> */}
 
   </React.StrictMode>,
 )
